b06e6d0a7d47f94b8555f1f6a123e8a406c08af4
jenabrushabhanu39 <jenabrushabhanu39@gmail.com>
2025-05-19 15:45:53 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=123456
+let accountEmail="brushabhanu.jena@intelegenica.com"
+var accountPassword="1234"
+accountCity="Kolkata"
+let accountState;
+
+// accountId=2 //not allowed
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail="rajuabhai@gmail.com"
+accountPassword="24789"
+accountCity="Bhubaneswar"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"User Strict"; //treat all JS code as newer version
+
+//alert (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3
+)  //code readability should be high, I mean do not write like this
+
+console.log("Raju")
